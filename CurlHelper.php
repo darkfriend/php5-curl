787a5ca56b949f57a3bfc05d42f7c2c09733bb5c
darkfriend <maslov.tlt@gmail.com>
2019-12-20 22:33:01 +0300

    begin curl helper

---
+++ CurlHelper.php
@@ -0,0 +1,299 @@
+<?php
+
+namespace darkfriend\helpers;
+
+/**
+ * CurlHelper - php5 curl helper package
+ * @package darkfriend\helpers
+ * @version 1.0.0
+ * @author darkfriend <hi@darkfriend.ru>
+ */
+class CurlHelper
+{
+    /** @var self */
+    protected static $_instance;
+    /** @var resource */
+    protected $_ch;
+
+    /** @var array */
+    protected $headers = array();
+    /** @var int */
+    protected $timeout = 60;
+    /** @var bool */
+    protected $debug = false;
+    /** @var string */
+    protected $debugFile = '';
+    /** @var int  */
+    protected $port;
+    /** @var array */
+    protected $curlProperties;
+
+    /** @var int */
+    public $lastCode = 0;
+    /** @var string */
+    public $lastHeaders = '';
+    /** @var string */
+    public $lastError;
+
+    /** @var string */
+    public $acceptLanguage = 'ru-RU';
+
+    /**
+     * Singleton
+     * @param bool $newSession create new instance
+     * @param array $options
+     * @return self
+     * @throws \Exception
+     */
+    public static function getInstance($newSession = false, $options = [])
+    {
+        if (!\function_exists('curl_init')) {
+            throw new \Exception('Curl is not found!');
+        }
+        if (!self::$_instance || $newSession) {
+            self::$_instance = new self($options);
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * CurlHelper constructor.
+     * @param array $options
+     */
+    protected function __construct($options = [])
+    {
+        if ($options) {
+            foreach ($options as $key => $option) {
+                if (\substr($key, 0, 1) == '_') continue;
+                if (isset($this->{$key})) {
+                    $this->{$key} = $option;
+                }
+            }
+        }
+    }
+
+    /**
+     * Set http headers
+     * @param array $headers key=>$value
+     * @return $this
+     * @example [[$headers]] ['Accept-Language'=>'en-US']
+     */
+    public function setHeaders(array $headers)
+    {
+        $this->headers = array_merge($this->headers, $headers);
+        return $this;
+    }
+
+    /**
+     * Set request port
+     * @param int $port
+     * @return $this
+     */
+    public function setPort(int $port)
+    {
+        $this->port = $port;
+        return $this;
+    }
+
+    /**
+     * Set curl properties
+     * @param array $properties
+     * @return $this
+     */
+    public function setCurlProperties($properties)
+    {
+        if(!$properties) return $this;
+        $this->curlProperties = \array_merge(
+            $this->curlProperties,
+            $properties
+        );
+        return $this;
+    }
+
+    /**
+     * Get curl properties
+     * @return array
+     */
+    public function getCurlProperties()
+    {
+        return $this->curlProperties;
+    }
+
+    /**
+     * Cleared curl properties
+     * @return $this
+     */
+    public function clearCurlProperties()
+    {
+        $this->curlProperties = array();
+        return $this;
+    }
+
+    /**
+     * Set headers to curl
+     * @return $this
+     */
+    protected function initHeaders()
+    {
+        if (empty($this->headers['Accept-Language'])) {
+            $this->headers['Accept-Language'] = $this->acceptLanguage;
+        }
+        $headers = array();
+        foreach ($this->headers as $key => $header) {
+            if ($header) {
+                $headers[] = "$key: $header";
+            }
+        }
+        \curl_setopt(
+            $this->_ch,
+            \CURLOPT_HTTPHEADER,
+            $headers
+        );
+        return $this;
+    }
+
+    /**
+     * Return curl-resource and set headers
+     * @param string $method http-method
+     * @return false|resource
+     * @throws \Exception
+     */
+    protected function initCurl($method = 'post')
+    {
+        if (!\function_exists('curl_init')) {
+            throw new \Exception('curl is not found!');
+        }
+        if (empty($this->_ch)) {
+            $this->_ch = \curl_init();
+            \curl_setopt($this->_ch, \CURLOPT_RETURNTRANSFER, true);
+            \curl_setopt($this->_ch, \CURLOPT_FOLLOWLOCATION, true);
+            if($this->port) {
+                \curl_setopt($this->_ch, \CURLOPT_PORT, $this->port);
+            }
+            switch ($method) {
+                case 'get': break;
+                case 'post':
+                    \curl_setopt($this->_ch, \CURLOPT_POST, 1);
+                    break;
+                default:
+                    \curl_setopt($this->_ch, \CURLOPT_CUSTOMREQUEST, $method);
+                    break;
+            }
+            \curl_setopt($this->_ch, \CURLOPT_HEADER, 1);
+            \curl_setopt($this->_ch, \CURLOPT_SSL_VERIFYPEER, 0);
+            \curl_setopt($this->_ch, \CURLOPT_TIMEOUT, $this->timeout);
+            \curl_setopt($this->_ch, \CURLOPT_CONNECTTIMEOUT, $this->timeout);
+        }
+        if($this->curlProperties) {
+            \curl_setopt_array($this->_ch, $this->curlProperties);
+        }
+        return $this->_ch;
+    }
+
+    /**
+     * Do request
+     * @param string $url
+     * @param array $data request data
+     * @param string $method request method (post)
+     * @param string $requestType request content-type (text)
+     * @param string $responseType response content-type (json)
+     * @return mixed
+     * @throws \Exception
+     */
+    public function request($url, $data = array(), $method = 'post', $requestType = '', $responseType = 'json')
+    {
+        $this->clear();
+        $this->initCurl($method);
+        if($data) {
+            if ($requestType == 'json') {
+                $data = \json_encode($data, \JSON_UNESCAPED_UNICODE | \JSON_UNESCAPED_SLASHES);
+            } else {
+                $data = \http_build_query($data);
+            }
+            if($method == 'get') {
+                $url .= '?' . $data;
+            } else {
+                \curl_setopt($this->_ch, \CURLOPT_POSTFIELDS, $data);
+            }
+        }
+        \curl_setopt($this->_ch, \CURLOPT_URL, $url);
+
+        if ($requestType == 'json') {
+            $this->headers['Content-Type'] = 'application/json; charset=utf-8';
+        } elseif (!empty($requestType)) {
+            $this->headers['Content-Type'] = "$requestType; charset=utf-8";
+        }
+
+        $this->initHeaders();
+
+        if ($this->debug) {
+            $this->debug(array(
+                'debugEvent' => 'before-request',
+                'url' => $url,
+                'requestHeaders' => $this->headers,
+                'requestData' => $data,
+                'requestType' => $requestType,
+                'responseType' => $responseType,
+            ));
+        }
+
+        $response = \curl_exec($this->_ch);
+
+        $header_size = \curl_getinfo($this->_ch, \CURLINFO_HEADER_SIZE);
+        $this->lastHeaders = \substr($response, 0, $header_size);
+        $this->lastCode = (int) \curl_getinfo($this->_ch, \CURLINFO_HTTP_CODE);
+
+        \curl_close($this->_ch);
+
+        $body = \substr($response, $header_size);
+
+        if ($this->debug) {
+            $this->debug(array(
+                'debugEvent' => 'after-request',
+                'url' => $url,
+                'requestHeaders' => $this->headers,
+                'requestData' => $data,
+                'code' => $this->lastCode,
+                'responseHeaders' => $this->lastHeaders,
+                'requestType' => $requestType,
+                'responseType' => $responseType,
+                'body' => $body,
+            ));
+        }
+
+        if ($responseType == 'json' && $body) {
+            $body = \json_decode($body, true);
+        }
+
+        if ($this->debug) {
+            $this->debug(array(
+                'debugEvent' => 'result',
+                'body' => $body,
+            ));
+        }
+
+        return $body;
+    }
+
+    /**
+     * Clear last request
+     */
+    protected function clear()
+    {
+        $this->lastCode = 0;
+        $this->lastError = '';
+        $this->lastHeaders = '';
+        unset($this->_ch);
+    }
+
+    /**
+     * Debug
+     * @param mixed $data
+     * @return void
+     */
+    protected function debug($data)
+    {
+        DebugHelper::traceInit('curl', DebugHelper::TRACE_MODE_APPEND, $this->debugFile);
+        DebugHelper::trace($data);
+    }
+}
---
+++ README.md
@@ -0,0 +1,146 @@
+# PHP5-Helpers - классы хелперы, которые часто бывают полезны в разработке
+
+**Структура:**
+* DebugHelper
+  * ```DebugHelper::$mainKey``` - свойство, содержащее имя ключа для $_COOKIE и $_GET
+  * ```DebugHelper::print_pre($o,$die,$show)``` - статичный метод, который выводит всю структуру массива и объекта, с информацией о файле и строке (подробности ниже)
+  * ```DebugHelper::call($func,$params)``` - статичный метод, который вызывает переданную функцию только у админа, передавая нужные параметры (подробности ниже)
+  * ```DebugHelper::trace($message,$category='common')``` - статичный метод трессировки (ниже примеры использования)
+* StringHelper
+  * ```StringHelper::htmlspecialchars($val)``` - статичный метод, который делает htmlspecialchars() для строк и массивов
+  * ```StringHelper::htmlspecialchars_decode($val)``` - статичный метод, который делает htmlspecialchars_decode() для строк и массивов
+  * ```StringHelper::generateString($length,$chars)``` - статичный метод, который возвращает сгенерированную строку нужной длины
+  * ```StringHelper::getDeclension($value,$words)``` - статичный метод, который возвращает окончания слов при слонении. _Например: 5 товаров, 1 товар, 3 товара_
+  
+* ArrayHelper
+  * ``ArrayHelper::in_array($needle, $haystack)`` - highload method for search value in array
+  * ``ArrayHelper::isMulti($arr)`` - check array on multiple array
+  * `` ArrayHelper::sortValuesToArray($sourceArray,$orderArray)`` - Sort values array to order array
+  * `` ArrayHelper::sortKeysToArray($sourceArray,$orderArray)`` - Sort keys source array to order array
+  
+## DebugHelper::print_pre($o,$die,$show);
+* $o - данные, которые надо вывести
+* $die - прерывать ли после вывода выполнение скрипта (по умолчанию false)
+* $show - выводить всем [или только в определенных случаях] (по умолчанию true)
+
+### Пример
+```php
+use \darkfriend\devhelpers\DebugHelper;
+$data = [
+  'key1' => 'value1',
+  'key2' => 'value2',
+  'key3' => [
+    'subKey1' => 'subValue1',
+    'subKey2' => 'subValue2',
+  ],
+];
+DebugHelper::print_pre($data);
+```
+
+## DebugHelper::call($func,$params)
+* $func - функция, которую надо вывести
+* $params - массив параметров которые надо передать
+
+### Пример
+```php
+use \darkfriend\devhelpers\DebugHelper;
+$data = [
+  'key1' => 'value1',
+  'key2' => 'value2',
+  'key3' => [
+    'subKey1' => 'subValue1',
+    'subKey2' => 'subValue2',
+  ],
+];
+
+// способ 1: используя $params
+DebugHelper::call(function($data) {
+  DebugHelper::print_pre($data);
+},$data);
+
+// способ 2: используя use
+DebugHelper::call(function() use ($data) {
+  DebugHelper::print_pre($data);
+});
+```
+## DebugHelper::trace($message,$category)
+* $message - сообщение
+* $category - категория трассировки
+
+### Дополнительные возможности
+
+* поддержка режимов трассировния
+    * TRACE_MODE_REPLACE - режим перезаписи лога
+    * TRACE_MODE_APPEND - режим дополнение лога
+    * TRACE_MODE_SESSION - режим trace-сессии
+* поддержка trace-сессий - каждый запуск в отдельном 
+
+
+### Example 1: простая трассировка
+_Задача: Простая запись данных в лог_
+```php
+use \darkfriend\devhelpers\DebugHelper;
+$array1 = [
+  'key1' => 'value1',
+  'key2' => 'value2'
+];
+
+// trace 1
+DebugHelper::trace($array1);
+// итог: запишет $array1 с категорией common.
+
+$array1['key3'] = [
+  'subKey1' => 'subValue1',
+  'subKey2' => 'subValue2',
+];
+
+// trace 2
+DebugHelper::trace($array1);
+// итог: допишет в лог обновленный $array1 с категорией common
+```
+
+#### Example 1: FAQ
+
+* _Где лежит файл?_ - путь ``$_SERVER['DOCUMENT_ROOT].'/trace.log'``
+* _Что будет в логе?_ - будет 2 записи переменной $array1. По умолчанию идет запись лога сверху вниз
+* _Какая категория будет?_ - по умолчанию категория "common"
+
+### Example 2: каждый запуск в отдельный файл
+_Задача: Мы сохраняем данные и хотим трассировать id-строки и сохраняемые данные_
+
+```php
+use \darkfriend\devhelpers\DebugHelper;
+
+$id = 1; // идентификатор
+
+// делаем инициализацию
+// $id - ключ trace-session
+// self::TRACE_MODE_SESSION - включаем режим trace-session
+DebugHelper::traceInit($id, DebugHelper::TRACE_MODE_SESSION);
+
+$array1 = [
+  'key1' => 'value1',
+  'key2' => 'value2',
+  'key3' => 'value3'
+];
+
+DebugHelper::trace($array1);
+// итог: запишет $array1 с категорией common.
+
+$array1['key3'] = [
+  'subKey1' => 'subValue1',
+  'subKey2' => 'subValue2',
+];
+
+// trace 2
+DebugHelper::trace($array1);
+// итог: допишет в лог обновленный $array1 с категорией common
+```
+
+#### Example 2: FAQ
+
+* _Где лежит файл?_ - путь ``$_SERVER['DOCUMENT_ROOT]."/{$id}-trace.log"``
+* _Что будет в логе?_ - будет 2 записи переменной $array1. По умолчанию идет запись лога сверху вниз
+* _Какая категория будет?_ - по умолчанию категория "common"
+* _Как изменить путь до лога?_ - по умолчанию лог создается в корне, чтоб его изменить, нужно передать путь от корня в 3-ий параметр метода DebugHelper::traceInit(). Пример: ``DebugHelper::traceInit($id, self::TRACE_MODE_SESSION,'/logs')``
+* _Могу ли я для одного trace сделать один файл, для другого - другой?_ - да, нужно в нужный момент вызвать метод ``DebugHelper::setHashSession($hash)``, где $hash - это любой ключ.
